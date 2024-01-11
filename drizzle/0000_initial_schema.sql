c643371cddda6599afc0edc0426c836c4f90afed
SeeuSim <seeusimong@gmail.com>
2024-01-11 14:33:09 +0800

    Add schema for chat truncation
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- drizzle/0000_initial_schema.sql
+++ drizzle/0000_initial_schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "embeddings" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"createdTime" timestamp DEFAULT now(),
+	"created_time" timestamp DEFAULT now(),
 	"content" text,
 	"metadata" jsonb,
 	"embedding" vector(768),
@@ -20,17 +20,18 @@ CREATE TABLE IF NOT EXISTS "messages" (
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "room" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"createdTime" timestamp DEFAULT now(),
-	"modifiedTime" timestamp DEFAULT now(),
+	"created_time" timestamp DEFAULT now(),
+	"modified_time" timestamp DEFAULT now(),
 	"summary" text,
+	"truncate_index" integer DEFAULT 0,
 	CONSTRAINT "room_id_unique" UNIQUE("id")
 );
 --> statement-breakpoint
-CREATE INDEX IF NOT EXISTS "time_index" ON "messages" ("time_stamp");--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "time_stamp_index" ON "messages" ("time_stamp");--> statement-breakpoint
 
-CREATE UNIQUE INDEX embeddings_meta_unique ON "embeddings" (("metadata"->>'title'),("metadata"->>'splitNumber'));--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "embeddings_meta_unique" ON "embeddings" (("metadata"->>'title'),("metadata"->>'splitNumber'));--> statement-breakpoint
 
-CREATE INDEX IF NOT EXISTS "modifiedIndex" ON "room" ("modifiedTime" DESC);--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "modified_time_index" ON "room" ("modified_time");--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "messages" ADD CONSTRAINT "messages_room_id_room_id_fk" FOREIGN KEY ("room_id") REFERENCES "public"."room"("id") ON DELETE cascade ON UPDATE cascade;
 EXCEPTION
