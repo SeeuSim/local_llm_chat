3fa89690a1aa532b054ff12870c1641f0caf64de
SeeuSim <seeusimong@gmail.com>
2024-01-12 06:26:07 +0800

    Add unlinking action for removing document from room
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- src/components/layouts/chat-layout/NavBar.tsx
+++ src/components/layouts/chat-layout/NavBar.tsx
@@ -1,16 +1,44 @@
 'use client';
 
+import { Cross1Icon } from '@radix-ui/react-icons';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { forwardRef, useContext } from 'react';
 
+import type { IAPIDocumentsLinkParams } from '@/app/api/documents/link/types';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/components/ui/tooltip';
 import { chatRoomContext } from '@/lib/contexts/chatRoomContext';
+import { searchParamsRoomIdContext } from '@/lib/contexts/chatRoomSearchParamsContext';
 import { cn } from '@/lib/utils';
 
 import DarkModeToggle from './DarkModeToggle';
 import { Logo } from './Logo';
 import { MobileSideNav } from './MobileSideNav';
+import { Button } from '@/components/ui/button';
 
-export const NavBar = forwardRef<HTMLDivElement>((props, ref) => {
+export const NavBar = forwardRef<HTMLDivElement>((_props, ref) => {
+  const queryClient = useQueryClient();
+  const { roomId } = useContext(searchParamsRoomIdContext);
   const { documents } = useContext(chatRoomContext);
+
+  const { mutate: linkUnlinkDocuments, isPending } = useMutation({
+    mutationFn: async (params: { isLinkUnlink: boolean; document: string }) => {
+      const payload: IAPIDocumentsLinkParams = {
+        roomId,
+        isLinkUnlink: params.isLinkUnlink,
+        documentTitles: [params.document],
+      };
+      return fetch('/api/documents/link', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+      });
+    },
+    onSuccess() {
+      queryClient.refetchQueries({ queryKey: ['chat', 'documents', 'get', roomId] });
+    },
+  });
 
   return (
     <header
@@ -29,13 +57,43 @@ export const NavBar = forwardRef<HTMLDivElement>((props, ref) => {
             <div
               key={document}
               className={cn(
-                'max-w-[200px] flex-shrink-0 truncate rounded-md border border-border bg-background px-2 py-1 text-xs',
+                'flex max-w-[200px] flex-shrink-0 flex-row gap-1 truncate rounded-md border border-border bg-background px-2 py-1 text-xs',
                 index === 0 && 'hidden sm:flex',
                 index === 1 && 'hidden md:flex',
                 index === 2 && 'hidden lg:flex'
               )}
             >
-              <span>{document}</span>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <span className='flex-1 truncate'>{document}</span>
+                  </TooltipTrigger>
+                  <TooltipContent className='border border-border bg-secondary-foreground p-1 text-xs text-secondary shadow-sm'>
+                    {document}
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      disabled={isPending}
+                      onClick={() => {
+                        linkUnlinkDocuments({
+                          isLinkUnlink: false,
+                          document,
+                        });
+                      }}
+                      className='m-0 h-min bg-transparent p-0 text-primary hover:bg-transparent hover:text-primary'
+                    >
+                      <Cross1Icon className='hover:scale-110 hover:text-red-500' />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent className='border border-border bg-secondary-foreground p-1 text-xs text-secondary shadow-sm'>
+                    <span>Unlink document from this chat</span>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
             </div>
           ))}
         </div>
